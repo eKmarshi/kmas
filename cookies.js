7e048726bde32edebcad7970b9fe1394a6838887
eKmarshi <50351499+eKmarshi@users.noreply.github.com>
2019-05-13 14:52:24 +0500

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#efefef",
+      "text": "#404040"
+    },
+    "button": {
+      "background": "#8ec760",
+      "text": "#ffffff"
+    }
+  },
+  "theme": "classic",
+  "content": {
+    "message": "We use cookies to improve your experience on our website. By browsing this website, you agree to our use of cookies.",
+    "dismiss": "Accept",
+    "link": "Read more",
+    "href": "https://kmarshub.blogspot.com/2010/07/privacy-policy.html"
+  }
+})});
+/*
+display: inline-block;
+    width: 100px;
+    box-sizing: border-box;
+    padding: 10px;
+    margin: 5px;
+    font-size: 12px;
+    line-height: 16px;
+    color: #FFF;
+    text-align: center;
+    -moz-border-radius: 20px;
+    -webkit-border-radius: 20px;
+    border-radius: 20px;
+    background: #00a1c6;
+    */
