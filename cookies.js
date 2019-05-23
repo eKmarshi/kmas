12c263f0556d79729eaacef085621e561dc835d2
eKmarshi <50351499+eKmarshi@users.noreply.github.com>
2019-05-23 12:38:00 +0500

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -12,13 +12,17 @@ window.cookieconsent.initialise({
   },
   "theme": "classic",
   "content": {
-    "message": "We use cookies to improve your experience on our website. By browsing this website, you agree to our use of cookies.",
+    "message": "KMARSHub is a plateform for learning, testing and traning of Tutorials, references and examples relating to HTML, CSS, JavaScript, JSON, PHP, Python, AngularJS, SQL, Bootstrap, Node.js, jQuery, XQuery, AJAX, and XML. Therefore KMARSHub use cookies to improve your experience on our website. By browsing this website, you agree to our use of cookies.",
     "dismiss": "Accept",
     "link": "Read more",
     "href": "https://kmarshub.blogspot.com/2010/07/privacy-policy.html"
   }
 })});
 /*
+-moz-border-radius: 20px;
+    -webkit-border-radius: 20px;
+    border-radius: 20px;
+    background: #00a1c6;
 display: inline-block;
     width: 100px;
     box-sizing: border-box;
@@ -28,8 +32,4 @@ display: inline-block;
     line-height: 16px;
     color: #FFF;
     text-align: center;
-    -moz-border-radius: 20px;
-    -webkit-border-radius: 20px;
-    border-radius: 20px;
-    background: #00a1c6;
     */
